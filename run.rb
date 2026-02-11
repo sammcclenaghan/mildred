979e646f038f0f7ef4978e2aa0bfee057318fbc8
Sam McClenaghan <sam@aream.ca>
2026-02-11 15:45:28 -0800

    listfiles

--- run.rb
+++ run.rb
@@ -3,7 +3,7 @@ require_relative "src/agent"
 
 RubyLLM.configure do |config|
   config.openai_use_system_role = true
-  config.openai_api_base = "http://localhost:11434/v1"
+  config.openai_api_base = ENV.fetch("OLLAMA_API_BASE", "http://localhost:11434/v1")
   config.openai_api_key = "dummy-key"
 end
 
--- src/agent.rb
+++ src/agent.rb
@@ -1,11 +1,12 @@
 require "ruby_llm"
 
 require_relative "tools/read_file"
+require_relative "tools/list_files"
 class Agent
   def initialize
     @chat = RubyLLM.chat(model: "granite4:latest", provider: :openai, assume_model_exists: true)
     @chat.with_instructions "You are a file organizer who will be given tasks and you will use tool calls to implement them, be super concise, we need almost no output from you"
-    @chat.with_tools(Tools::ReadFile)
+    @chat.with_tools(Tools::ReadFile, Tools::ListFiles)
       .on_tool_call do |tool_call|
         puts "Calling tool: #{tool_call.name}"
         puts "Arguments: #{tool_call.arguments}"
